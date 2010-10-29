955c407faf205588b7091bf926aecf5879c408b1
asgerhoedt <asgerhoedt@gmail.com>
2010-10-29 11:51:14 +0000

    Convert methods

--- Utils/CUDA/Convert.h
+++ Utils/CUDA/Convert.h
@@ -35,12 +35,30 @@ namespace OpenEngine {
                     return out.str();
                 }
 
+                static string ToString(const int4& t){
+                    std::ostringstream out;
+                    out << "(";
+                    out << t.x << ", ";
+                    out << t.y << ", ";
+                    out << t.z << ", ";
+                    out << t.w << ")";
+                    return out.str();
+                }
+
                 static string ToString(const dim3& t){
                     std::ostringstream out;
                     out << "(";
                     out << t.x << ", ";
                     out << t.y << ", ";
                     out << t.z << ")";
+                    return out.str();
+                }
+
+                static string ToString(const float2& t){
+                    std::ostringstream out;
+                    out << "(";
+                    out << t.x << ", ";
+                    out << t.y << ")";
                     return out.str();
                 }
 
@@ -89,6 +107,19 @@ namespace OpenEngine {
                     return out.str();
                 }
 
+                static string ToString(int4* vl, unsigned int size){
+                    int4 tmp[size];
+                    cudaMemcpy(tmp, vl, size * sizeof(int4), cudaMemcpyDeviceToHost);
+                    CHECK_FOR_CUDA_ERROR();
+
+                    std::ostringstream out;
+                    out << "[" << ToString(tmp[0]);
+                    for (unsigned int i = 1; i < size; ++i)
+                        out << ", " << ToString(tmp[i]);
+                    out << "]";
+                    return out.str();
+                }
+
                 static string ToString(float* vl, unsigned int size){
                     float tmp[size];
                     cudaMemcpy(tmp, vl, size * sizeof(float), cudaMemcpyDeviceToHost);
@@ -98,6 +129,19 @@ namespace OpenEngine {
                     out << "[" << tmp[0];
                     for (unsigned int i = 1; i < size; ++i)
                         out << ", " << tmp[i];
+                    out << "]";
+                    return out.str();
+                }
+
+                static string ToString(float2* vl, unsigned int size){
+                    float2 tmp[size];
+                    cudaMemcpy(tmp, vl, size * sizeof(float2), cudaMemcpyDeviceToHost);
+                    CHECK_FOR_CUDA_ERROR();
+
+                    std::ostringstream out;
+                    out << "[" << ToString(tmp[0]);
+                    for (unsigned int i = 1; i < size; ++i)
+                        out << ", " << ToString(tmp[i]);
                     out << "]";
                     return out.str();
                 }
